3b644a3acf15844c416258bf72740d91e6f5dc8d
JunYong Sun <ssjy4974@naver.com>
2023-08-02 12:16:51 +0900

    20230802

--- day02/src/day02/Main.java
+++ day02/src/day02/Main.java
@@ -1,10 +1,36 @@
 package day02;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			System.out.println("Input cmd ....");
+			String cmd = sc.next();
+			if (cmd.equals("q")) {
+				System.out.println("Bye ...");
+				break;
+			} else if (cmd.equals("a")) {
+				System.out.println(cmd);
+			} else if (cmd.equals("b")) {
+				while (true) {
+					System.out.println("Input Cmd2 ...");
+					String cmd2 = sc.next();
+					if (cmd2.equals("q")) {
+						System.out.println("Bye2 ...");
+						break;
+					} else if (cmd2.equals("a2")) {
+						System.out.println(cmd2);
+					}
+				}
+
+			}
+		}
+		sc.close();
+		System.out.println("End ...");
 	}
 
 }
